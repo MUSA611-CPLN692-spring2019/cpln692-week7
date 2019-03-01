309230881c5b646d9441bd8e8698f50a76f7242e
Mayu Tanaka <mayutanaka@berkeley.edu>
2019-03-01 15:29:58 -0500

    lab1 complete

--- labs/lab1/js/main.js
+++ labs/lab1/js/main.js
@@ -40,104 +40,65 @@ https://macwright.org/2015/03/23/geojson-second-bite.html
 https://medium.com/@sumit.arora/what-is-geojson-geojson-basics-visualize-geojson-open-geojson-using-qgis-open-geojson-3432039e336d
 
 
-
-
 ===================== */
 
 
 // Task 1.1 - add the five markers from squares.json to the map using the methods
 // we've used so far in class: _.map or _.each, forEach()
+var mapMarkers = function(data) {
+  var markers = [];
+    _.each(data, function(feature) {
+      var m = L.marker([Number(feature.LAT), Number(feature.LNG)]).bindPopup(feature.NAME);
+      markers.push(m);
+      m.addTo(map);
+  });
+  return markers;
+};
 
+var squaresJson_markers = mapMarkers(squaresJson);
 
 // Task 1.2 - remove squares.json from the map
 // iterating over each marker  using map.removeLayer(point)
+var removeMarkers = function(marks) {
+    _.each(marks, function(m) { map.removeLayer(m); });
+};
 
-
-// markers = squaresJson.map((point) => {
-//   return L.marker([point.LAT, point.LNG]).addTo(map);
-// })
-//
-//
-// markers.forEach((point) => {
-//   map.removeLayer(point)})
-
-
-
-// layer.addTo(map);
-// map.removeLayer(squaresPoly)
-
-
-// _.forEach(squaresJson,(point) => {
-//   return L.marker([point.LAT, point.LNG]).addTo(map)
-// });
-
-
-
-
-
-
-
+removeMarkers(squaresJson_markers);
 
 // task 2.1 - add squares.geojson to the map
-//  Try: L.geoJSON().addTo(map);
-
+var squaresGeoJson_markers = L.geoJSON(squaresGeoJson).addTo(map);
 
 // task 2.2 - remove squares.geojson from the map
-// Try: map.removeLayer()
-
-
+map.removeLayer(squaresGeoJson_markers);
 
 // task 3 - filter by some property on squares.geojson
-
-
-// L.geoJSON(squaresGeoJson, {
-//     filter: function(feature) {
-//         return feature.properties.DOB_NAMESAKE == 1674;
-//     }
-// }).addTo(map);
-
-
-
+var filtered_squaresGeoJson_markers = L.geoJSON(squaresGeoJson, {
+  filter: function(d) { return d.properties.INDEGO_STATION; }
+}).addTo(map);
 
 // task 4.1 - add squaresPoly.geojson to the map
+var layerPoly = L.geoJSON(squaresPoly).addTo(map);
+map.removeLayer(layerPoly);
+
 // task 4.2 - add conditional coloring to squaresPoly.geojson
-
-
-
+layerPoly = L.geoJSON(squaresPoly, {
+  style: function (feature) {
+    switch (feature.properties.TYPE) {
+      case 'circle': return {color: '#000000'};
+      case 'square': return {color: '#FF00FF'};
+      default: return {color: '#0000FF'};
+    }
+  }
+});
 
 // task 5 - add two buttons to the sidebar from javascript using jquery: (1) add layer (2) remove layer
-
-
 $('<br><input type="button" id="btnToAdd" value="add" />').appendTo($(".sidebar"));
-
 $('<br><input type="button" id="btnToRemove" value="remove" />').appendTo($(".sidebar"));
 
-
 $( "#btnToAdd" ).click(function() {
-  layer.addTo(map);
+  layerPoly.addTo(map);
 });
-
 
 $( "#btnToRemove" ).click(function() {
-  map.removeLayer(layer);
+  map.removeLayer(layerPoly);
 });
-
-
-
-
-
-// var myStyle = function(feature) {
-//   if (feature.properties.INDEGO_STATION == true) {
-//   return {fillColor: 'red'};
-// } else if (feature.properties.COLLDAY == "MON") {
-// return {fillColor: 'green'}
-// } else return {fillColor: 'yellow'}
-// };
-
-
-var myStyle = function(feature) {
-  if (feature.properties.INDEGO_STATION == true) {
-    console.log("trues")
-  return {fillColor: 'green'};
-} else return {fillColor: 'red'}
-};
