4bb0016b166d86b4522fd8088bbedb0eb5d8e0bb
Mayu Tanaka <mayutanaka@berkeley.edu>
2019-03-14 23:56:20 -0400

    assignment filled out

--- assignment/midtermFunctions.js
+++ assignment/midtermFunctions.js
@@ -1,3 +1,49 @@
 /* ================================
 Week 7 Assignment: Midterm Functions + Signatures
 ================================ */
+
+// Figures out which function to display feature by type
+function mapType(feature) {
+  switch(feature.type) {
+    case "museum":
+      museum(feature);
+      break;
+    case "grocery":
+      grocery(feature);
+      break;
+    case "transit":
+      transit(feature);
+      break;
+    default:
+      general(feature);
+  }
+}
+
+// Create marker and add to map (change marker size, color, shape by properties)
+// general properties: distance, type
+function general(feature) {
+  var marker = L.marker(feature.coordinates).bindPopUp(feature.name);
+}
+
+// Create custom marker for museum and add to map
+// unique properties: rating
+function museum(feature) {
+  var marker = L.marker(feature.coordinates).bindPopUp(feature.name);
+}
+
+// Create custom marker for grocery and add to map
+// unique properties: stories, has-housing, year-built
+function grocery(feature) {
+  var marker = L.marker(feature.coordinates).bindPopUp(feature.name);
+}
+
+// Create custom marker for transit and add to map
+// unique properties: transit-type
+function transit(feature) {
+  var marker = L.marker(feature.coordinates).bindPopUp(feature.name);
+}
+
+// Take in markers to remove and remove them - requires global list(?) of markers
+function remover(markers) {
+  // remove layer
+}
--- assignment/slideModel.js
+++ assignment/slideModel.js
@@ -5,13 +5,33 @@ Week 7 Assignment: Slide Model
 /** Here's a simple 'model' of a slide.
  *  It tracks the slide's index and the title we want in our HTML
  */
-var slideExample = {
-  slideNumber: 1,
-  title: "My first slide",
-  filter: function(geojsonFeature) { return true }
-};
+// var slideExample = {
+//   slideNumber: 1,
+//   title: "My first slide",
+//   filter: function(geojsonFeature) { return true; }
+// };
 
 /** Here's the simplest implementation I could come up with for
  * representing a deck of slides (nothing exotic is necessary!)
  */
-var slideDeck = [slideExample1, slideExample2, slideExample3]
+// var slideDeck = [slideExample1, slideExample2, slideExample3];
+
+class Slide {
+  constructor(order, title) {
+    this.order = order;
+    this.title = title;
+    this.first = false;
+    this.last = false;
+  }
+  filter() { return true; } //filter executes here rather than returning function
+  setToFirst() { this.first = true; }
+  setToLast() { this.last = true; }
+}
+
+var slide1 = Slide(1, "First Slide");
+var slide2 = Slide(2, "Second Slide");
+var slide3 = Slide(3, "Third Slide");
+var slide4 = Slide(4, "Fourth Slide");
+var slide5 = Slide(5, "Fifth Slide");
+
+var slideDeck = [slide1, slide2, slide3, slide4, slide5];
